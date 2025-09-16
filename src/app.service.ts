11a24960f9ab7a31f38b26ce3722376af45e6b5d
Alan Leonel Maciel <103143446+AlanLeonelMaciel@users.noreply.github.com>
2025-09-16 02:14:20 +0000

    se instala como dependencia class-validator y se crea CreateUserDTO

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola devOps!';
+    return `
+      <div style="display:flex;justify-content:center;align-items:center;height:100vh;background:#222;">
+        <h1 style="color:#61dafb;font-family:sans-serif;font-size:3rem;">
+          Hola <span style="color:#fff;">devOps!</span>
+        </h1>
+      </div>
+    `;
   }
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @MinLength(6)
+  @IsNotEmpty()
+  readonly password: string;
+}
