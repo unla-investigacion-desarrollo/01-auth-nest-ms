c30952bc4ed81dc220192778ade9250defb1aa30
Alan Leonel Maciel <103143446+AlanLeonelMaciel@users.noreply.github.com>
2025-09-16 03:21:47 +0000

    se crea el registro de un usuario con mongo

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -10,4 +10,4 @@ export class User {
   @Prop({ required: true })
   password: string;
 }
-export const UserSchema=SchemaFactory.createForClass(User);
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const newUser=new this.userModel(createUserDto);
+      return await newUser.save();
+    } catch (error) {
+      throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findAll() {
