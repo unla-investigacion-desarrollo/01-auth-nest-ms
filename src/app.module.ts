b3bb5c4ae42445c51b7183559a3efe5729304e0d
Alan Leonel Maciel <103143446+AlanLeonelMaciel@users.noreply.github.com>
2025-09-16 02:29:36 +0000

    se instala mongoose y se configura en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'docker/local/.env',
+      isGlobal: true,
+    }),
+    UserModule,
+  MongooseModule.forRoot(process.env.MONGO_URI || ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
